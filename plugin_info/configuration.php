2bf20b6c392717c73733b76dddf93b8a0a94a41f
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2026-02-21 19:21:24 +0100

    Sync default socket port to 55113
    Update the tooltip in plugin_info/configuration.php to show the correct default internal socket port (55113 instead of 55200). This fixes a mismatch with the input placeholder and avoids confusion about the default port.

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -97,7 +97,7 @@ $_labelBranchSSHM = _branchLabel($_branchSSHManager);
         <div class="form-group">
             <label class="col-md-3 control-label">{{Port Socket Interne}}
                 <sup><i class="fas fa-exclamation-triangle tooltips" style="color:var(--al-warning-color)!important;" title="{{Le démon devra être redémarré après la modification de ce paramètre}}"></i></sup>
-                <sup><i class="fas fa-question-circle tooltips" title="{{[ATTENTION] Ne changez ce paramètre qu'en cas de nécessité. (Défaut = 55200)}}"></i></sup>
+                <sup><i class="fas fa-question-circle tooltips" title="{{[ATTENTION] Ne changez ce paramètre qu'en cas de nécessité. (Défaut = 55113)}}"></i></sup>
             </label>
             <div class="col-md-1">
                 <input class="configKey form-control" data-l1key="socketPort" placeholder="55113" />
